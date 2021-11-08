8fca3e46a1669a5dd6b11bf2f9bf086de3797907
nyximos <nyximos@gmail.com>
2021-11-08 16:10:08 +0900

    Initial commit

---
+++ src/main/java/com/ramen/RamenShop/SpringBootRamenShopApplication.java
@@ -0,0 +1,13 @@
+package com.ramen.RamenShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRamenShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRamenShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramen/RamenShop/SpringBootRamenShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramen.RamenShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRamenShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
