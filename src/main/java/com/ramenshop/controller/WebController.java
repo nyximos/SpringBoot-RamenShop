581ce00b5753ce7c82a21b417f341635321420a1
Beopard <93901813+Beopard@users.noreply.github.com>
2021-11-11 09:30:44 +0900

    package명

--- src/main/java/com/ramenshop/controller/WebController.java
+++ src/main/java/com/ramenshop/controller/WebController.java
@@ -1,4 +1,4 @@
-package com.ramen.RamenShop.controller;
+package com.ramenshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
