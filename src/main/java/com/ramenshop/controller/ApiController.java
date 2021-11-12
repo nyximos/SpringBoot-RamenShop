506be5997010e4253b14c5cf888f931a938fb832
Beopard <93901813+Beopard@users.noreply.github.com>
2021-11-12 16:13:50 +0900

    회원가입

--- src/main/java/com/ramenshop/controller/ApiController.java
+++ src/main/java/com/ramenshop/controller/ApiController.java
@@ -1,5 +1,11 @@
 package com.ramenshop.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -10,14 +16,31 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ramenshop.data.Admin;
 import com.ramenshop.data.Menu;
 import com.ramenshop.data.Result;
+import com.ramenshop.repository.AdminRepository;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
+	@Autowired
+	AdminRepository adminRepository;
+	
+	@Bean
+	public PasswordEncoder passwordEncode() {
+		return new BCryptPasswordEncoder();
+	}
+
 	
 	@PostMapping("/admin/members")
 	public Result regMember(@RequestBody Admin admin ) {
-		return new Result("ok");
+		System.out.println("컨트롤러안");
+		Optional<Admin> a = adminRepository.findById(admin.getAdminId());
+		if(a.isEmpty()==true){
+			admin.setPassword(passwordEncode().encode(admin.getPassword()));
+			adminRepository.save(admin);
+			return new Result("ok");			
+		}
+		return new Result("no");
+
 	}
 	@DeleteMapping("/admin/members")
 	public Result delMember(@RequestBody Admin admin ) {
