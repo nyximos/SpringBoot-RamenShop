78bf1f57fea44b2762456679b13ea8187b3b82a6
Beopard <93901813+Beopard@users.noreply.github.com>
2021-11-16 09:03:03 +0900

    메뉴추가2
    미완

--- src/main/java/com/ramenshop/controller/WebController.java
+++ src/main/java/com/ramenshop/controller/WebController.java
@@ -1,9 +1,16 @@
 package com.ramenshop.controller;
 
+import java.io.File;
+
+import javax.swing.filechooser.FileSystemView;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class WebController {
@@ -13,8 +20,20 @@ public class WebController {
 		return "index";
 	}
 	
+	
+	@PostMapping("/upload")
+	public String uploadSingle(@RequestParam("files") MultipartFile file) throws Exception {
+	    String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
+	    String basePath = rootPath + "/" + "single";
+	    String filePath = basePath + "/" + file.getOriginalFilename();
+	    File dest = new File(filePath);
+	    file.transferTo(dest); // 파일 업로드 작업 수행
+	    return "uploaded";
+	}
+	
 	@GetMapping("/login")
 	public String login() {
+		
 		return "login";
 	}
 	
@@ -56,6 +75,7 @@ public class WebController {
 	
 	@GetMapping("/admin/sales")
 	public String sales(Model model) {
+		String root = request.getContextPath();
 		return "sales";
 	}
 	
