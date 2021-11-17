eec463f887b3ab4ade07d0da811b7bad97106e47
nyximos <nyximos@gmail.com>
2021-11-17 20:12:49 +0900

    리다이렉트

--- src/main/java/com/ramenshop/controller/MenuApiController.java
+++ src/main/java/com/ramenshop/controller/MenuApiController.java
@@ -2,11 +2,15 @@ package com.ramenshop.controller;
 
 import java.io.File;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ramenshop.data.Menu;
 import com.ramenshop.data.MenuGroup;
@@ -24,15 +28,19 @@ public class MenuApiController {
     		@RequestParam(name="price") int price,
     		@RequestParam(name="discription") String discription,
     		@RequestParam(name="imgFile") MultipartFile imgFile,
-    		@RequestParam(name="menuGroupId") String menuGroupId
+    		@RequestParam(name="menuGroupId") String menuGroupId,
+    		HttpServletRequest request,
+    		HttpServletResponse response,
+    		ModelAndView mav
     		) {
     	
     	//String name, int price, String discription, String imgUrl, Long menuGroupId
     	
     	
         try { // 이미지 경로를 DB에 셋팅
-            String baseDir = "C:\\Users\\mycom\\Documents\\GitHub\\SpringBoot-RamenShop\\src\\main\\resources\\static\\imgs\\";
-            String filePath = baseDir + imgFile.getOriginalFilename();
+            String baseDir = request.getSession().getServletContext().getRealPath("\\"); //webapp까지 경로
+            System.out.println(baseDir);
+            String filePath = baseDir + "\\imgs\\" + imgFile.getOriginalFilename();
             imgFile.transferTo(new File(filePath)); // 해당 경로에 이미지 파일 저장
             
             MenuGroup menuGroup = new MenuGroup();
@@ -42,6 +50,9 @@ public class MenuApiController {
             Menu menu = new Menu(name, price,discription, menuGroup, imgName);
             menu.setImgUrl(filePath);
             menuService.saveMenu(menu);
+            response.sendRedirect("list");
+            
+
             return "등록성공";
          } catch(Exception e) {
             e.printStackTrace();
