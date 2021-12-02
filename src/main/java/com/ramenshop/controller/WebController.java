82b826ef0001d0dded3ea9106461c42a56dd75c7
nyximos <nyximos@gmail.com>
2021-12-02 17:00:10 +0900

    값 표시

--- src/main/java/com/ramenshop/controller/WebController.java
+++ src/main/java/com/ramenshop/controller/WebController.java
@@ -27,33 +27,12 @@ public class WebController {
 
 	
 	@GetMapping("/")
-	public String index(HttpServletResponse response) {
+	public String index() {
 		
-		//쿠키 객체 생성, "cookieName"이라는 이름으로 쿠키를 생성하고, 그 값은 "cookieValue"로 설정
-		Cookie rememberCookie = new Cookie("cookieName", "cookieVlaue"); 
-		
-		// 쿠키 경로 설정, "/"는 모든 경로에서 사용하겠다는 뜻
-		rememberCookie.setPath("/");
-		
-		// 쿠키를 유지할 시간 설정(단위 : 초)
-		rememberCookie.setMaxAge(60*5);
-		
-		response.addCookie(rememberCookie);
 
-	
 		return "index";
 	}
-	
-	
-//	@PostMapping("/upload")
-//	public String uploadSingle(@RequestParam("files") MultipartFile file) throws Exception {
-//	    String rootPath = FileSystemView.getFileSystemView().getHomeDirectory().toString();
-//	    String basePath = rootPath + "/" + "single";
-//	    String filePath = basePath + "/" + file.getOriginalFilename();
-//	    File dest = new File(filePath);
-//	    file.transferTo(dest); // 파일 업로드 작업 수행
-//	    return "uploaded";
-//	}
+
 	
 	@GetMapping("/login")
 	public String login() {
