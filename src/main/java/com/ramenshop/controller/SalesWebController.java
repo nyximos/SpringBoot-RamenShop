4cc7b4f3a865a5089be64e000b6a68b303dfacea
Beopard <93901813+Beopard@users.noreply.github.com>
2021-12-04 15:00:44 +0900

    메뉴별 기간별 매출표

--- src/main/java/com/ramenshop/controller/SalesWebController.java
+++ src/main/java/com/ramenshop/controller/SalesWebController.java
@@ -1,16 +1,73 @@
 package com.ramenshop.controller;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ramenshop.repository.OrderMenuRepository;
+import com.ramenshop.service.MenuService;
 
 @Controller
 public class SalesWebController {
 	
+	@Autowired
+	OrderMenuRepository orderMenuRepository;
+	@Autowired
+	MenuService menuService;
+	
 	@GetMapping("/admin/sales")
-	public String sales(Model model) {
-//		String root = request.getContextPath();
+	public String sales(
+			Model model
+//			@RequestParam String menu
+			) {
 		
+		String fromdate = "211201";
+		String todate = "211231";
+		String menu= "콜라";
+		
+		int a = 211201;
+		int b = 211205;
+		LocalDate ld1 = LocalDate.now();
+		LocalDate ld2 = LocalDate.now();
+		LocalDateTime d = LocalDateTime.parse("2021-12-31 23:59:59", 
+				DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+		
+		model.addAttribute("menus", menuService.findMenus());
+		model.addAttribute("sales", orderMenuRepository.selectAllSQL());
+		
+		String length = "1";
+		model.addAttribute("daySales", orderMenuRepository.selectAllSQL(length));
+		length = "7";
+		model.addAttribute("weekSales", orderMenuRepository.selectAllSQL(length));
+		length = "30";
+		model.addAttribute("monthSales", orderMenuRepository.selectAllSQL(length));
+		length = "365";
+		model.addAttribute("yearSales", orderMenuRepository.selectAllSQL(length));
+		
+		
+		return "sales";
+	}
+	
+	@PostMapping("/admin/sales")
+	public String selectedSales(
+			Model model,
+			@RequestParam String menu,
+			@RequestParam String fromdate,
+			@RequestParam String todate
+			) {
+		System.out.println(menu);
+		System.out.println(fromdate);
+		System.out.println(todate);
+		
+		model.addAttribute("menus", menuService.findMenus());
+		model.addAttribute("sales", orderMenuRepository.selectAllSQL(fromdate,todate,menu));
 		
 		return "sales";
 	}
--- src/main/java/com/ramenshop/repository/OrderMenuRepository.java
+++ src/main/java/com/ramenshop/repository/OrderMenuRepository.java
@@ -1,11 +1,35 @@
 package com.ramenshop.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.ramenshop.data.OrderMenu;
 
 @Repository
 public interface OrderMenuRepository extends JpaRepository<OrderMenu, Long> {
-
+	
+	@Query(value = "SELECT d.name, d.price, SUM(b.count) 판매수량, d.price*SUM(b.count) 판매액, SUM(b.order_menu_price)-d.price*SUM(b.count) 옵션판매액, SUM(b.order_menu_price) 총판매액 FROM order_menu b\r\n"
+			+ "INNER JOIN menu d ON b.menu_id =d.menu_id\r\n"
+			+ "INNER JOIN ORDERs a ON a.order_id = b.order_id\r\n"
+			+ "WHERE a.order_time BETWEEN :fromdate and :todate AND d.name = :menu\r\n"
+			+ "GROUP BY d.menu_id", nativeQuery = true)
+	public List<Object> selectAllSQL(@Param("fromdate") String fromdate,@Param("todate") String todate
+			,@Param("menu") String menu);
+	
+	@Query(value = "SELECT d.name, d.price, SUM(b.count) 판매수량, d.price*SUM(b.count) 판매액, SUM(b.order_menu_price)-d.price*SUM(b.count) 옵션판매액, SUM(b.order_menu_price) 총판매액 FROM order_menu b\r\n"
+			+ "INNER JOIN menu d ON b.menu_id =d.menu_id\r\n"
+			+ "INNER JOIN ORDERs a ON a.order_id = b.order_id\r\n"
+			+ "GROUP BY d.menu_id", nativeQuery = true)
+	public List<Object> selectAllSQL();
+	
+	@Query(value = "SELECT SUM(b.order_menu_price) 총판매액 FROM order_menu b\r\n"
+			+ "INNER JOIN ORDERs a ON a.order_id = b.order_id\r\n"
+			+ "WHERE DATE(a.order_time) >= DATE_SUB(NOW(), INTERVAL :length DAY)", nativeQuery = true)
+	public List<Object> selectAllSQL(@Param("length")String length);
+	
 }
